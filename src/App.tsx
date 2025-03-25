3cd36887f8f4cf76d8991cdd06fa85cdf963552f
risha328 <rishamondal328@gmail.com>
2025-03-25 21:19:59 +0530

    upadated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,47 @@
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AllProductPage from './pages/AllProductPage';
-import ProductPage from './pages/ProductPage';
-import { CartProvider } from './context/CartContext';
-import Navbar from './components/Navbar';
-import { CartDrawer } from './components/CartDrawer';
-import { useState } from 'react';
-import { CheckoutPage } from './pages/Checkout';
-import Footer from './components/Footer';
-import KishanParivarPage from './pages/KishanParivarPage';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import HomePage from "./pages/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AllProductPage from "./pages/AllProductPage";
+import ProductPage from "./pages/ProductPage";
+import { CartProvider } from "./context/CartContext";
+import Navbar from "./components/Navbar";
+import { CartDrawer } from "./components/CartDrawer";
+import { useState } from "react";
+import { CheckoutPage } from "./pages/Checkout";
+import Footer from "./components/Footer";
+import KishanParivarPage from "./pages/KishanParivarPage";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import AdminLayout from "./layout/AdminLayout";
 import OverviewPage from "./pages/admin/OverviewPage";
-// import ProductPage from "./pages/admin/ProductPage";
 
 function App() {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  
+
   return (
-    
-      <BrowserRouter>
+    <BrowserRouter>
       <CartProvider>
         <Navbar onCartClick={() => setIsCartOpen(true)} />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/all-products" element={<AllProductPage/>}/>
-          <Route path="/product/:id" element={<ProductPage/>}  />
+          <Route path="/all-products" element={<AllProductPage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
           <Route path="/checkout" element={<CheckoutPage />} />
           <Route path="/kishanParivarPage" element={<KishanParivarPage />} />
-          <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
 
-        {/* Admin Routes with Persistent Sidebar & Navbar */}
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<OverviewPage />} />
-          <Route path="products" element={<ProductPage />} />
-        </Route>
+          {/* Admin Routes */}
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<OverviewPage />} />
+            <Route path="products" element={<ProductPage />} />
+          </Route>
         </Routes>
-          <CartDrawer isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
-          <Footer />
-      </CartProvider>    
-      </BrowserRouter>
-      
-    
+
+        <CartDrawer isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
+        <Footer />
+      </CartProvider>
+    </BrowserRouter>
   );
 }
 
