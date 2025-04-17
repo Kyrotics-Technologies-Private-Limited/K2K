77cbd2cec2d03fe69c0f8ea72d9fc51638c70356
risha328 <rishamondal328@gmail.com>
2025-04-17 19:51:59 +0530

    admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,95 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { useState } from "react";
+import { CartProvider } from "./context/CartContext";
+import { AuthProvider } from "./context/AuthContext";
+
+// Public components
+import AdvertisementBar from "./components/common/AdvertisementBar";
+import Navbar from "./components/common/Navbar";
+import { CartDrawer } from "./components/common/CartDrawer";
+import Footer from "./components/common/Footer";
+
+// Public pages
 import HomePage from "./pages/HomePage";
 import AllProductPage from "./pages/AllProductPage";
 import ProductPage from "./pages/ProductPage";
-import { CartProvider } from "./context/CartContext";
-import { AuthProvider } from "./context/AuthContext";
-import Navbar from "./components/common/Navbar";
-import { CartDrawer } from "./components/common/CartDrawer";
-import { useState } from "react";
-import { CheckoutPage } from "./pages/Checkout";
-import Footer from "./components/common/Footer";
+//import CheckoutPage from "./pages/Checkout";
 import KishanParivarPage from "./pages/KishanParivarPage";
 import SignUp from "./pages/authPages/SignUp";
 import Login from "./pages/authPages/Login";
-import AdminLayout from "./layout/AdminLayout";
-import OverviewPage from "./pages/admin/OverviewPage";
 import OurStory from "./pages/OurStory";
-import AdvertisementBar from "./components/common/AdvertisementBar";
-import PrivacyPolicy from "./pages/policyPages/privacypolicy";
-import TermsOfService from "./pages/policyPages/TermsOfService";
-import ShippingPolicy from "./pages/policyPages/ShippingPolicy";
-import RefundPolicy from "./pages/policyPages/RefundPolicy";
 import Traceability from "./pages/Traceability";
 import OrdersPage from "./pages/OrdersPage";
 import OrderDetailPage from "./pages/OrderDetailPage";
 import ForgotPasswordPage from "./pages/authPages/ForgotPassword";
 import ResetPasswordPage from "./pages/authPages/ResetPassword";
 import UserProfilePage from "./pages/authPages/ProfilePage";
-//import AddressPage from "./pages/AddressPage";
-//import PhoneAuth from "./components/PhoneAuth";
-// import ProductPage from "./pages/admin/ProductPage";
 
-function App() {
+// Policy pages
+import PrivacyPolicy from "./pages/policyPages/privacypolicy";
+import TermsOfService from "./pages/policyPages/TermsOfService";
+import ShippingPolicy from "./pages/policyPages/ShippingPolicy";
+import RefundPolicy from "./pages/policyPages/RefundPolicy";
+
+// Admin components
+import Layout from "./components/admin/layout/Layout";
+import AdminDashboard from "./pages/admin/Admindashboard";
+import AdminProducts from "./pages/admin/AdminProducts";
+
+const PublicLayout = () => {
   const [isCartOpen, setIsCartOpen] = useState(false);
 
+  return (
+    <>
+      <AdvertisementBar />
+      <Navbar onCartClick={() => setIsCartOpen(true)} />
+      <main>
+        <Outlet />
+      </main>
+      <CartDrawer isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
+      <Footer />
+    </>
+  );
+};
+
+function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <CartProvider>
-          <AdvertisementBar />
-          <Navbar onCartClick={() => setIsCartOpen(true)} />
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/all-products" element={<AllProductPage />} />
-            <Route path="/product/:id" element={<ProductPage />} />
-            <Route path="/checkout" element={<CheckoutPage />} />
-            <Route path="/kishanParivarPage" element={<KishanParivarPage />} />
-            <Route path="/register" element={<SignUp />} />
-            {/* <Route path="/login" element={<Login />} /> */}
-            <Route path='/forgot-password' element={<ForgotPasswordPage/>}/>
-            <Route path='/reset-password' element={<ResetPasswordPage/>}/>
-            <Route path="/orders" element={<OrdersPage/>}/>
-            <Route path="/orders/:id" element={<OrderDetailPage/>}/>
-            <Route path='/our-story' element={<OurStory/>}/>
-            <Route path='/traceability' element={<Traceability/>}/>
-            <Route path='/privacypolicy' element={<PrivacyPolicy/>}/>
-            <Route path='/termsofservice' element={<TermsOfService/>}/>
-            <Route path='/shipping' element={<ShippingPolicy/>}/>
-            <Route path='/refund' element={<RefundPolicy/>}/>
-            <Route path='/profile' element={<UserProfilePage/>}/>
+            {/* Public routes with common layout */}
+            <Route element={<PublicLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/all-products" element={<AllProductPage />} />
+              <Route path="/product/:id" element={<ProductPage />} />
 
-            {/* Admin Routes */}
-            <Route path="/admin" element={<AdminLayout />}>
-              <Route index element={<OverviewPage />} />
-              <Route path="products" element={<ProductPage />} />
+              <Route
+                path="/kishanParivarPage"
+                element={<KishanParivarPage />}
+              />
+              <Route path="/register" element={<SignUp />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+              <Route path="/reset-password" element={<ResetPasswordPage />} />
+              <Route path="/orders" element={<OrdersPage />} />
+              <Route path="/orders/:id" element={<OrderDetailPage />} />
+              <Route path="/our-story" element={<OurStory />} />
+              <Route path="/traceability" element={<Traceability />} />
+              <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+              <Route path="/termsofservice" element={<TermsOfService />} />
+              <Route path="/shipping" element={<ShippingPolicy />} />
+              <Route path="/refund" element={<RefundPolicy />} />
+              <Route path="/profile" element={<UserProfilePage />} />
+            </Route>
+
+            {/* Admin routes with separate layout */}
+            <Route path="/admin" element={<Layout />}>
+              <Route index element={<AdminDashboard />} />
+              <Route path="products" element={<AdminProducts />} />
+              {/* Add other admin routes as needed */}
             </Route>
           </Routes>
-
-          <CartDrawer
-            isOpen={isCartOpen}
-            onClose={() => setIsCartOpen(false)}
-          />
-          <Footer />
         </CartProvider>
       </AuthProvider>
     </BrowserRouter>
