df41e4f244dc96ea3218869a6d82648560c2dc37
arghyadip700 <arghyadipdhara1@gmail.com>
2025-04-20 18:30:13 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -82,7 +82,6 @@ function App() {
               <Route path="products" element={<ProductPage />} />
             </Route>
           </Routes>
-      <Footer />
         </AppProvider>
       </BrowserRouter>
     </Provider>
